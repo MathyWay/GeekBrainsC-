9ce1f82309fa4540bec3898ed5d8b61d1c40d8eb
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-10 20:56:42 +0300

    Wrote two methods how to pow number in 2

--- Square console/Program.cs
+++ Square console/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Квадрат числа " + value + " равен " + value * value);
+
+Console.WriteLine("Второй вариант: Квадрат числа " + value + " равен " + Math.Pow(value, 2));
