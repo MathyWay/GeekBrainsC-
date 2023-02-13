ea112d23fc330f3376d6a56bfb8a69c6464455ea
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-13 22:43:39 +0300

    Added index search

--- Maximum Search/Program.cs
+++ Maximum Search/Program.cs
@@ -1,12 +1,24 @@
-﻿int MAX(int ret, int b, int c)
-{
-    if(b > ret) ret = b;
-    if(c > ret) ret = c;
-    return ret;
-}
+﻿//int MAX(int ret, int b, int c)
+//{
+//    if(b > ret) ret = b;
+//    if(c > ret) ret = c;
+//    return ret;
+//}
 int[] array = {23, 34, 1, 231, 1222, -333333, 12, 643, 1245};
 
-int max = MAX(MAX(array[0], array[1], array[2]),
-            MAX(array[3], array[4], array[5]),
-            MAX(array[6], array[7], array[8]));
-Console.WriteLine(max);
+//int max = MAX(MAX(array[0], array[1], array[2]),
+//            MAX(array[3], array[4], array[5]),
+//            MAX(array[6], array[7], array[8]));
+
+int find = 12;
+int index = 0;
+while (index < array.Length)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+Console.WriteLine(index);
