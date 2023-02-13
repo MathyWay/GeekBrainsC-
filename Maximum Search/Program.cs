495e962fd7cef8942acdcfff6b760e30ad0dc9f4
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-13 22:24:47 +0300

    Added Maximumm search

---
+++ Maximum Search/Program.cs
@@ -0,0 +1,21 @@
+﻿int a1 = 21;
+int a2 = -37;
+int a3 = 123;
+int a4 = 43;
+int a5 = 877;
+int a6 = -1002;
+int a7 = 246;
+int a8= 2;
+int a9 = 333;
+
+int max = a1;
+if(a2 > max) max = a2;
+if(a3 > max) max = a3;
+if(a4 > max) max = a4;
+if(a5 > max) max = a5;
+if(a6 > max) max = a6;
+if(a7 > max) max = a7;
+if(a8 > max) max = a8;
+if(a9 > max) max = a9;
+
+Console.WriteLine(max);
