e4f23db6c098c34ea803567fc433534ecb0a1a42
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-13 23:01:32 +0300

    Wrote Filling and printing array

---
+++ Array library/Program.cs
@@ -0,0 +1,26 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int len = col.Length;
+    int position = 0;
+    while(position < len)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
