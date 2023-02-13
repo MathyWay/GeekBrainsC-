09bcb43b0e20e2cd17c6ae12841d63c16057498f
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-13 22:29:48 +0300

    Wrote functional maximum search

--- Maximum Search/Program.cs
+++ Maximum Search/Program.cs
@@ -1,21 +1,20 @@
-﻿int a1 = 21;
+﻿int MAX(int ret, int b, int c)
+{
+    if(b > ret) ret = b;
+    if(c > ret) ret = c;
+    return ret;
+}
+int a1 = 21;
 int a2 = -37;
 int a3 = 123;
 int a4 = 43;
-int a5 = 877;
+int a5 = 100002;
 int a6 = -1002;
 int a7 = 246;
 int a8= 2;
 int a9 = 333;
 
-int max = a1;
-if(a2 > max) max = a2;
-if(a3 > max) max = a3;
-if(a4 > max) max = a4;
-if(a5 > max) max = a5;
-if(a6 > max) max = a6;
-if(a7 > max) max = a7;
-if(a8 > max) max = a8;
-if(a9 > max) max = a9;
-
+int max = MAX(MAX(a1, a2, a3),
+            MAX(a4, a5, a6),
+            MAX(a7, a8, a9));
 Console.WriteLine(max);
