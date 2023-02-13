f4f0c2442c009b4a02fd2f198cd031cd3d517c9e
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-13 23:11:38 +0300

    Found index of random value in random array

--- Array library/Program.cs
+++ Array library/Program.cs
@@ -3,7 +3,7 @@
     int index = 0;
     while(index < collection.Length)
     {
-        collection[index] = new Random().Next(1, 100);
+        collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
@@ -18,7 +18,26 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int index = 0;
+    int position = -1;
+    while (index < collection.Length)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
 PrintArray(array);
+
+int pos = IndexOf(array, 6);
+Console.WriteLine("Index: " + pos);
