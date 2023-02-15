009e6e472a92a7acd0f398a7c20f5584eb700225
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-16 01:13:24 +0300

    Wrote EvenNumbersUmtilN in Homework160223_4

---
+++ Homeworks/Homework160223_4/Program.cs
@@ -0,0 +1,9 @@
+﻿int a, i = 1;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+while(i < a + 1)
+{
+    if(i % 2 == 0) Console.Write(i + ", ");
+    i++;
+}
+Console.WriteLine();
