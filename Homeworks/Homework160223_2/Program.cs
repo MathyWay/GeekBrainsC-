ab80c4622666f51e2d0d6929a7c0bd1a09813b9d
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-16 00:52:46 +0300

    Wrote second task in Homework160223

--- Homeworks/Homework160223_2/Program.cs
+++ Homeworks/Homework160223_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int MAX(int a1, int a2)
+{
+    if (a1 > a2) return a1;
+    else return a2;
+}
+int a, b, c;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+c = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(MAX(MAX(a, b), c));
