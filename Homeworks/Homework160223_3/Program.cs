4f26ed360223eb0d8cbbe5d1e7ec039df0e42a2a
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-16 01:00:27 +0300

    Wrote IsEven in Homework160223_3

---
+++ Homeworks/Homework160223_3/Program.cs
@@ -0,0 +1,5 @@
+﻿int a;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if(a % 2 == 0) Console.WriteLine("Число " + a + " чётное");
+else Console.WriteLine("Число " + a + " нечётное");
