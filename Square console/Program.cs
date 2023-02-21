4ed48f086fc4135d4af9c0d1a4fd6afcde861cfb
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-21 21:01:17 +0300

    add Sorce commit

--- Square console/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿Console.Clear();
-
-Console.Write("Введите число: ");
-int value = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Квадрат числа " + value + " равен " + value * value);
-
-Console.WriteLine("Второй вариант: Квадрат числа " + value + " равен " + Math.Pow(value, 2));
