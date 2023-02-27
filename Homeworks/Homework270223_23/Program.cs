b9ab6bcce820ae334de4ffe54b558e4647bc1f62
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-28 00:28:20 +0300

    Wrote GetCube program

---
+++ Homeworks/Homework270223_23/Program.cs
@@ -0,0 +1,9 @@
+﻿int number;
+int i = 1;
+Console.Write("Введите число: ");
+number = Convert.ToInt32(Console.ReadLine());
+while(i <= number)
+{
+    Console.Write($"{Math.Pow(i, 3)}" + (i != number ? ", " : "\n"));
+    i++;
+}
