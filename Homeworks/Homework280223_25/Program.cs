9803ea7083337ecd801dba2ef6a18230f9aa5a89
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-28 18:27:29 +0300

    Wrote MyPow with cycle

---
+++ Homeworks/Homework280223_25/Program.cs
@@ -0,0 +1,17 @@
+﻿int PowMy(int number1, int number2)
+{
+    int result = 1;
+    for (int i = 0; i < number2; i++)
+    {
+        result *= number1;
+    }
+    return result;
+}
+
+int a, b;
+Console.Write("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{a}^{b} = {PowMy(a, b)}");
