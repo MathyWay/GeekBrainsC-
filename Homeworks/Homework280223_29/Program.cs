4e9ec469c9d4c212f027063efb07bb11ca68dbe9
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-28 18:51:58 +0300

    Wrote FillArray

---
+++ Homeworks/Homework280223_29/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] FillArray()
+{
+    int[] array = new int[8];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next();
+    }
+    return array;
+}
+
+int[] arr = new int[8];
+arr = FillArray();
+
+foreach (var item in arr)
+{
+    Console.Write($"{item}" + ((item == arr[arr.Length - 1]) ? "\n" : ", "));
+}
