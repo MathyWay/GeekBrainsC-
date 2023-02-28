f7f1564b643be849d6d397b956f539c8b3fa2299
MathicsWay <MathyWay@users.noreply.github.com>
2023-02-28 18:33:52 +0300

    Wrote GetNumberSum

---
+++ Homeworks/Homework280223_27/Program.cs
@@ -0,0 +1,15 @@
+﻿int GetNumberSum(int number)
+{
+    int result = 0;
+    do
+    {
+        result += number % 10;
+        number = number / 10;
+    } while (number != 0);
+    return result;
+}
+
+int a;
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Сумма цифр в числе {a} = {GetNumberSum(a)}");
