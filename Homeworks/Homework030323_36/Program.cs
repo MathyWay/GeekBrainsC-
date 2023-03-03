b872e95f50e0bda35328c62de6f2810b83bc2bc2
MathicsWay <MathyWay@users.noreply.github.com>
2023-03-03 16:17:29 +0300

    Wrote homework CountMotEvenPos in array

---
+++ Homeworks/Homework030323_36/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+    }
+    return array;
+}
+
+int CountNotEvenPos(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i += 2)
+    {
+        count+= arr[i];
+    }
+    return count;
+}
+
+Console.Write("Количество элементов в массиве = ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[(number > 0 ? number : 0)];
+
+arr = FillArray(arr.Length);
+
+foreach (var item in arr)
+{
+    Console.Write($"{item}" + ((item == arr[arr.Length - 1]) ? "\n" : ", "));
+}
+Console.WriteLine($"Сумма чисел на нечетных позициях в массиве = {CountNotEvenPos(arr)}");
