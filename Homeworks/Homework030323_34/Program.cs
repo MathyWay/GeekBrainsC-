5513f28bda949c221f178a6344a0b4e41d8f9982
MathicsWay <MathyWay@users.noreply.github.com>
2023-03-03 16:09:42 +0300

    Wrote homework CountEvens in array

---
+++ Homeworks/Homework030323_34/Program.cs
@@ -0,0 +1,31 @@
+﻿int[] FillArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 999);
+    }
+    return array;
+}
+
+int CountEvens(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Количество элементов в массиве = ");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[(number > 0 ? number : 0)];
+
+arr = FillArray(arr.Length);
+
+foreach (var item in arr)
+{
+    Console.Write($"{item}" + ((item == arr[arr.Length - 1]) ? "\n" : ", "));
+}
+Console.WriteLine($"Количество чётных чисел в массиве = {CountEvens(arr)}");
